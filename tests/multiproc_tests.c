b30fa69ec0ef55f1cdba3af7a25f8cb9e16d49ea
Bartosz <bartosz347@gmail.com>
2019-01-16 12:47:09 +0100

    multi process tests

---
+++ tests/multiproc_tests.c
@@ -0,0 +1,74 @@
+#include <unistd.h>
+#include <wait.h>
+#include <stdlib.h>
+#include "utils.h"
+
+__pid_t newWriterChild();
+
+__pid_t newReaderChild();
+
+void readers() {
+    newReaderChild();
+    newReaderChild();
+    newReaderChild();
+}
+
+void writers() {
+    newWriterChild();
+    newWriterChild();
+    newWriterChild();
+}
+
+int main() {
+    system("rm ../tests/test-output/file02_*");
+    writeFileToSimpleFsDebug("../tests/test-files/file02", "/file02", 0);
+
+    printf("MAIN: starting processes\n");
+
+
+    readers();
+    //writers();
+
+
+    while (waitpid(-1, NULL, 0) > 0);
+
+    return 0;
+}
+
+__pid_t newWriterChild() {
+    __pid_t pid = fork();
+    if (pid == 0) {
+        __pid_t myPid = getpid();
+        printf("PID=%d started\n", myPid);
+        sleep(myPid % 5 + 1); // TODO
+
+        int writeStatus = writeFileToSimpleFsDebug("../tests/test-files/file02", "/file02", myPid);
+
+        printf("PID=%d write status: %d\n", myPid, writeStatus);
+        printf("PID=%d finished\n", myPid);
+        exit(0);
+    } else {
+        return pid;
+    }
+}
+
+
+__pid_t newReaderChild() {
+    __pid_t pid = fork();
+    if (pid == 0) {
+        __pid_t myPid = getpid();
+        printf("PID=%d started\n", myPid);
+        sleep(myPid % 5 + 1); // TODO
+        char dstFilename[] = "../tests/test-output/file02_00000000";
+        sprintf(dstFilename, "../tests/test-output/file02_%d", myPid);
+        printf("%s\n", dstFilename);
+
+        int readStatus = readFileFromSimpleFs("/file02", dstFilename, getFilesize("../tests/test-files/file02"));
+
+        printf("PID=%d read status: %d\n", myPid, readStatus);
+        printf("PID=%d finished\n", myPid);
+        exit(0);
+    } else {
+        return pid;
+    }
+}
